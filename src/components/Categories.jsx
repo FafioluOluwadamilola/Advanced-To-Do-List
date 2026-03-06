29db4a8bf33b87533be24a453a2a14d6d59d3db2
Fafiolu Oluwadamilola <info4dammyfaffy2022@gmail.com>
2026-03-06 16:13:37 +0100

    95% done with design & Media Query

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,9 +10,9 @@ const Categories = ({ filter, setFilter }) => {
     <div className='flex items-center mt-10 flex-col w-full'>
       <h1 className='text-2xl font-bold text-left background-clip'>Categories:</h1>
 
-        <div className='mt-2 flex items-center justify-center flex-wrap gap-2'>
+      <div className='categoriesQuery mt-2 flex items-center justify-center flex-wrap gap-2'>
 
-          <motion.div
+        <motion.div
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.9 }}
           onClick={() => setFilter("All")}
@@ -25,22 +25,22 @@ const Categories = ({ filter, setFilter }) => {
           📋 All
         </motion.div>
 
-          {categories.map((cat) => (
-            <motion.div
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.9 }}
-              key={cat.name}
-              onClick={() => setFilter(cat.name)}
-              className={`m-2 p-2 w-30 text-sm rounded-lg text-black cursor-pointer
+        {categories.map((cat) => (
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            key={cat.name}
+            onClick={() => setFilter(cat.name)}
+            className={`m-2 p-2 w-30 text-sm rounded-lg text-black cursor-pointer
               ${filter === cat.name
-                  ? `${cat.styles.gradient} text-white`
-                  : "bg-gray-100 text-gray-700 border border-gray-200 hover:bg-purple-200"
-                }`}
-            >
-              {cat.label}
-            </motion.div>
-          ))}
-        </div>
+                ? `${cat.styles.gradient} text-white`
+                : "bg-gray-100 text-gray-700 border border-gray-200 hover:bg-purple-200"
+              }`}
+          >
+            {cat.label}
+          </motion.div>
+        ))}
+      </div>
 
 
 
