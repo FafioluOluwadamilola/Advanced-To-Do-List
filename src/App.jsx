77a588719ae84ddbf8f0fc4f704cc58a8cb673f3
Fafiolu Oluwadamilola <info4dammyfaffy2022@gmail.com>
2026-03-13 21:35:31 +0100

    115% done with mobile animation

--- src/App.jsx
+++ src/App.jsx
@@ -89,9 +89,9 @@ function App() {
 
 
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
+      transition={{ duration: 0.2 }}
     >
       <div className='container'>
 
